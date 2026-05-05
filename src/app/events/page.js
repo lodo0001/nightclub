e75213ff78a7f11ae1a8dfa89d5d7108faa7c974
Louise <lodo0001@stud.kea.dk>
2026-05-05 08:33:21 +0200

    Tilføjer lidt af navbar

---
+++ src/app/events/page.js
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Det her er events siden</h1>
+    </div>
+  );
+}
