dcdaccc8c6a010dfa58b799f00daeb1532dfbf09
Louise <lodo0001@stud.kea.dk>
2026-05-07 12:44:38 +0200

    Statisk version af welcome..

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Newsletter from "@/components/forside/Newsletter";
+import WelcomeToNightClub from "@/components/forside/WelcomeToNightClub";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <WelcomeToNightClub />
       <Newsletter />
     </div>
   );
